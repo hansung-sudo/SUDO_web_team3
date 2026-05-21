2cf8c14970a0044d0d014d7ca3d3b0ec7e65a592
gransmile3-ai <gransmile3@gmail.com>
2026-05-21 17:48:44 +0900

    first push

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route("/")
 def index():
