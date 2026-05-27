bbb06413bad0ff7c99b17429be31b3986235e33d
gransmile3-ai <gransmile3@gmail.com>
2026-05-27 21:27:02 +0900

    connection route change, intro rough

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def index():
     return render_template("index.html")
 
+@app.route("/intro")
+def intro():
+    return render_template("intro.html")
+
+@app.route("/board")
+def board():
+    return render_template("board.html")
+
+@app.route("/apply")
+def apply():
+    return render_template("apply.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
