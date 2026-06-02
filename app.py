f26e9f2136a9133d0aa6c1a561da36a7deba44dd
jjji04 <jjji04@naver.com>
2026-06-02 15:46:21 +0900

    지원화면 디자인

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def index():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
 ##alsrms002 first commit
